46f214be06d6285ca18f67d2b3e65f6af053855d
Filipe Dias <filipediascontato@gmail.com>
2023-01-15 13:13:56 -0300

    Creates the checkout api route

---
+++ src/pages/api/checkout.ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { stripe } from '../../lib/stripe'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'POST') {
+    res.status(405).json({ error: 'Method not allowed.' })
+  }
+
+  const { priceId } = req.body
+
+  if (!priceId) {
+    return res.status(400).json({ error: 'You must send the product priceId.' })
+  }
+
+  const successUrl = `${process.env.NEXT_URL}/success`
+  const cancelUrl = `${process.env.NEXT_URL}/`
+
+  const checkoutSession = await stripe.checkout.sessions.create({
+    success_url: successUrl,
+    cancel_url: cancelUrl,
+    mode: 'payment',
+    line_items: [
+      {
+        price: priceId,
+        quantity: 1,
+      },
+    ],
+  })
+
+  return res.status(201).json({
+    checkoutUrl: checkoutSession.url,
+  })
+}
