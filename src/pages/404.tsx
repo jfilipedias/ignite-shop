58950d49b217d2bf1857fab354af8b93e816da44
Filipe Dias <filipediascontato@gmail.com>
2023-01-16 17:55:52 -0300

    Creates a not found page

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import { NotFoundContainer } from '../styles/pages/404'
+
+export default function NotFound() {
+  return (
+    <NotFoundContainer>
+      <h1>Página não encontrada</h1>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </NotFoundContainer>
+  )
+}
---
+++ src/styles/pages/404.ts
@@ -0,0 +1,30 @@
+import { styled } from '..'
+
+export const NotFoundContainer = styled('main', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  width: '100%',
+  maxWidth: 1180,
+  height: 480,
+  margin: '0 auto',
+
+  h1: {
+    marginTop: 64,
+    fontSize: '$2xl',
+    color: '$gray300',
+  },
+
+  a: {
+    display: 'block',
+    marginTop: '5rem',
+    color: '$green500',
+    fontSize: '$lg',
+    fontWeight: 'bold',
+    textDecoration: 'none',
+
+    '&:hover': {
+      color: '$green300',
+    },
+  },
+})
